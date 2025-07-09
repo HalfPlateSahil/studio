5d58a761c8c3f8c958b87fda571f6aa6a9be43dc
tejaswini.234306202 vcet <tejaswini.234306202@vcet.edu.in>
2025-07-09 19:17:56 +0000

    when i was deploying the updated files onto heroku, this error is coming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { ConceptCanvas } from '@/components/concept-canvas';
 
+export const dynamic = 'force-dynamic';
+
 export default function Home() {
   return (
     <main className="h-screen w-screen bg-background text-foreground overflow-hidden">
